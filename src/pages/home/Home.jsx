292d7d8fc32e49b8f026ce5c7044511934a39759
saalcazar <alcazar.samuel@gmail.com>
2024-06-22 15:41:53 -0400

    responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import './Home.css'
+import Social from './Social/Social'
 import Democracy from "./democracy/Democracy"
 import Education from "./educationDevelopment/Education"
 import Futbol from "./futbol/Futbol"
@@ -6,13 +8,14 @@ import Protection from "./protection/Protection"
 
 const Home = () => {
   return (
-    <>
+    <main className="container">
       <Education />
       <Protection />
       <Futbol />
       <Democracy />
       <Promotion />
-    </>
+      <Social />
+    </main>
   )
 }
 
---
+++ src/pages/home/Social/Social.jsx
@@ -0,0 +1,13 @@
+import './Social.css'
+const Social = () => {
+  return (
+    <main className="main-social">
+      <div class="fb-page" data-href="https://www.facebook.com/ASAT.Bolivia" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/ASAT.Bolivia" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ASAT.Bolivia">ASAT - Asociación Amigos de la Tierra</a></blockquote></div>
+      <aside className='links-social'>
+        <h2>Hola</h2>
+      </aside>
+    </main>
+  )
+}
+
+export default Social
